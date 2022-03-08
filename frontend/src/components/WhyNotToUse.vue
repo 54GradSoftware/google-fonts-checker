7362bac18c2f08f5a45c4c736aca055770993d91
adb-sh <git@adb.sh>
2022-03-08 15:52:39 +0100

    add text

--- frontend/src/components/WhyNotToUse.vue
+++ frontend/src/components/WhyNotToUse.vue
@@ -14,9 +14,18 @@
     Dadurch kann Google ein Nutzerprofil von dir anlegen und zum Beispiel relavante Werbung anzeigen.
     Dadurch verstößt die Nutzung von Google Fonts je nach Anwendungsfall meist gegen die DSGVO.
   </p>
-  <h3>Wie kann ich Google Schriftarten ohne Tracking benutzen?</h3>
+  <h3>Wie kann ich Google Schriftarten ohne Tracking und DSGVO-Konform benutzen?</h3>
   <p>
-    Die Google Schriftarten können mit wenigs Aufwand auch selbständig gehostet werden.
+    Die Google Schriftarten können mit wenig Aufwand auch selbständig gehostet werden.
+    Dadauch kann sich zusätzlich sogar die Performance der Webseite verbessern. <br>
+    Um die Google Schriftarten einzubinden gibt es ein Online-Tool, welches diesen Prozess erleichtert:
+    <a href="https://google-webfonts-helper.herokuapp.com/fonts/roboto?subsets=latin" target="_blank" rel="noreferrer">Google Webfonts Helper</a><br>
+    Hier müssen zunächst alle Typen ausgewählt werden, die benötigt werden. <br>
+    Darauf wird ein CSS-File generiert, welches auf der eigenen Seite eingebunden werden muss. -
+    Dabei lässt sich der Order-Prefix festlegen, wo die Schriftarten später gespeichert werden sollen. <br>
+    Jetzt muss nur noch die Zip-Datei mit den Schriftarten runtergeladen
+    und in den Webspace hochgeladen werden. Zum Beispiel mit einem FTP-Client. <br>
+    Bei der Verwendung von Git müssen die Schriften einfach zum Projekt hinzugefügt werden.
   </p>
 </template>
 
