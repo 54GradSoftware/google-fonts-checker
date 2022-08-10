c658cfddea3ebcda228972c5c0c9f65d9ee28194
adb-sh <git@adb.sh>
2022-08-10 13:05:44 +0200

    add plausible - privacy-friendly tracking

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker';
 import router from './router';
 import { createI18n }  from 'vue-i18n';
 import { messages } from './languages';
+import { VuePlausible } from 'vue-plausible';
 
 export const i18n = createI18n({
   fallbackLocale: {
@@ -20,4 +21,13 @@ export const i18n = createI18n({
 createApp(App)
   .use(router)
   .use(i18n)
+  .use(
+    (vue, options) => VuePlausible({ prototype: {}, ...vue }, options), // getter to compensate https://github.com/moritzsternemann/vue-plausible/issues/16
+    {
+      domain: 'google-fonts-checker.54gradsoftware.de',
+      enableAutoPageviews: true,
+      trackLocalhost: false,
+      apiHost: 'https://plausible.io',
+    },
+  )
   .mount('#app');
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
@@ -7,14 +9,19 @@ module.exports = defineConfig({
       favicon16: "/favicon.ico",
       appleTouchIcon: "/favicon.ico",
       maskIcon: "/favicon.ico",
-      msTileImage: "/favicon.ico"
-    }
+      msTileImage: "/favicon.ico",
+    },
   },
   terser: {
     terserOptions: {
       compress: {
-        drop_console: true
-      }
-    }
-  }
-})
+        drop_console: true,
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillPlugin(),
+    ]
+  },
+});
