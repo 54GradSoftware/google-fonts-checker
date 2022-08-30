f3e519998d5861d34df05c8f22b5f19f1f4c4b0f
timofo <timo.formella@illusion-factory.de>
2022-08-30 10:59:40 +0200

    remove plausible

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,22 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import { createI18n }  from 'vue-i18n';
+import { messages } from './languages';
 
-createApp(App).use(router).mount('#app');
+export const i18n = createI18n({
+  fallbackLocale: {
+    'en': ['en-US'],
+    'en-UK': ['en-US'],
+    'de': ['de-DE'],
+    default: 'de-DE',
+  },
+  messages,
+  allowComposition: true,
+  mode: 'composition',
+});
+
+createApp(App)
+  .use(router)
+  .use(i18n)
+  .mount('#app');
