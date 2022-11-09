074f410f78343c6bd647da19404e9871ca97952b
adb-sh <git@adb.sh>
2022-11-09 16:58:46 +0100

    also match http

--- worker/trackerList.js
+++ worker/trackerList.js
@@ -4,14 +4,14 @@ export const trackerList = [
     slug: 'google-fonts',
     url: 'https://fonts.google.com/',
     showDetails: true,
-    match: url => url.match(/^https:\/\/fonts\.googleapis\.com/)||url.match(/^https:\/\/fonts\.gstatic\.com/)
+    match: url => url.match(/^https?:\/\/fonts\.googleapis\.com/)||url.match(/^https?:\/\/fonts\.gstatic\.com/)
   },
   {
     name: 'Google reCAPTCHA',
     slug: 'google-recaptcha',
     url: 'https://developers.google.com/recaptcha/',
     showDetails: true,
-    match: url => url.match(/^https:\/\/www\.google\.com\/recaptcha\/api\.js/)||url.match(/^https:\/\/www\.gstatic\.com\/recaptcha\/releases/)
+    match: url => url.match(/^https?:\/\/www\.google\.com\/recaptcha\/api\.js/)||url.match(/^https?:\/\/www\.gstatic\.com\/recaptcha\/releases/)
   },
   {
     name: 'Google Maps',
@@ -19,19 +19,19 @@ export const trackerList = [
     url: 'https://developers.google.com/maps/',
     showDetails: true,
     match: url =>
-      url.match(/^https:\/\/www\.google\.com\/maps\/embed/)||
-      url.match(/^https:\/\/www\.google\.com\/maps\/vt/)||
-      url.match(/^https:\/\/maps\.googleapis\.com\/maps\/api/)||
-      url.match(/^https:\/\/maps\.googleapis\.com\/maps-api-v3\/api/)||
-      url.match(/^https:\/\/khms1\.googleapis\.com\/kh/)||
-      url.match(/^https:\/\/maps\.gstatic\.com\/maps-api-v3\/embed/)||
-      url.match(/^https:\/\/maps\.gstatic\.com\/mapfiles/)
+      url.match(/^https?:\/\/www\.google\.com\/maps\/embed/)||
+      url.match(/^https?:\/\/www\.google\.com\/maps\/vt/)||
+      url.match(/^https?:\/\/maps\.googleapis\.com\/maps\/api/)||
+      url.match(/^https?:\/\/maps\.googleapis\.com\/maps-api-v3\/api/)||
+      url.match(/^https?:\/\/khms1\.googleapis\.com\/kh/)||
+      url.match(/^https?:\/\/maps\.gstatic\.com\/maps-api-v3\/embed/)||
+      url.match(/^https?:\/\/maps\.gstatic\.com\/mapfiles/)
   },
   {
     name: 'Adobe Fonts',
     slug: 'adobe-fonts',
     url: 'https://use.typekit.com',
     showDetails: true,
-    match: url => url.match(/^https:\/\/use\.typekit\.net/)
-  }
+    match: url => url.match(/^https?:\/\/use\.typekit\.net\/af/)
+  },
 ];
