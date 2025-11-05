66b243e48d344ec703d222b2d32d96470cf0492a
ArturNiko <apapikian@54gradsoftware.de>
2025-11-05 20:09:26 +0100

    only german routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,26 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import SuccessView from '../views/SuccessView.vue';
 
-const routes = [
+const routes = [/*
   {
     path: '/',
     redirect: () => ({ path: `/${ navigator.language || navigator.userLanguage || 'de-DE' }` })
   },
-  {
-    path: '/:locale/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/:locale/success',
-    name: 'successDE',
-    component: SuccessView
-  }
-];
+    */
+    {
+        path: '/', name: 'home', component: HomeView
+    }, {
+        path: '/success', name: 'successDE', component: SuccessView
+    }, {
+        path: '/:pathMatch(.*)*', name: 'notfound', redirect: '/'
+    }];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
